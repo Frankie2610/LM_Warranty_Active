8eee23e52a68ca41c2cf19d102a8811b5d4db050
giaquyenvan <giaquyenvan@gmail.com>
2026-08-16 05:43:46 +0700

    update

--- api/warranty/index.js
+++ api/warranty/index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     return json(res, 200, {
       ok: true,
       service: "lm-warranty-vercel",
-      version: "8.3.0",
+      version: "8.9.0",
       auth: "shopify-customer-account",
       appProxy: false
     });
